b052dd0b660b1f6d9fbca8694fed0f86d1172156
Rashid Coder <raaashid2010@gmail.com>
2025-08-15 20:52:49 +0500

    Mini Changes

--- script.js
+++ script.js
@@ -15,3 +15,70 @@ buttons.forEach(btn => {
     }
   });
 });
+
+
+document.getElementById("phoneNumber").addEventListener("input", function () {
+  let value = this.value.replace(/[^0-9+ ]/g, "");
+
+  if (value.includes("+")) {
+    value = "+" + value.replace(/\+/g, "").replace(/^\+/, "");
+  }
+
+  let numbers = value.replace(/\D/g, "");
+  
+  let formatted = value.startsWith("+") ? "+" : "";
+  if (numbers.length > 0) formatted += numbers.substring(0, 3);       // 998
+  if (numbers.length > 3) formatted += " " + numbers.substring(3, 5); // 12
+  if (numbers.length > 5) formatted += " " + numbers.substring(5, 8); // 345
+  if (numbers.length > 8) formatted += " " + numbers.substring(8, 10);// 67
+  if (numbers.length > 10) formatted += " " + numbers.substring(10, 12); // 89
+
+  this.value = formatted.trim();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const api = "https://voice-connect.atabek.space/";
+// const form = document.querySelector("form");
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   const full_name = document.getElementById("fullName").value.trim();
+//   const phone = document.getElementById("phoneNumber").value.trim();
+//   const comment = document.getElementById("comment").value.trim();
+
+//   fetch( api , {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//       full_name,
+//       phone,
+//       comment
+//     })
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log("Server result:", data);
+//       alert("Sucsess!");
+//       form.reset();
+//     })
+//     .catch(error => {
+//       console.error("Error:", error);
+//       alert("Error");
+//     });
+// })
+
